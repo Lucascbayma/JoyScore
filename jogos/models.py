ffe7c8e58f8efc57ff2ba11894bf027c44b9a2e9
Mateus Lins <maateuslfarias@gmail.com>
2025-09-18 12:30:02 -0300

    atualizado a classe avaliar e adicionado class biblioteca no models

--- jogos/models.py
+++ jogos/models.py
@@ -2,17 +2,31 @@ from django.db import models
 
 # Create your models here.
 
-class buscar(models.Model):
+class Buscar(models.Model):
     titulo = models.CharField(max_length=200)
     desenvolvedor = models.CharField(max_length=200, blank=True)
-    ano_lancamento = models.DateField(blank=True)
+    ano_lancamento = models.DateField(blank=True,null=True)
     genero = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return self.titulo
     
-class avaliar(models.Model):
-    nota = models.IntegerField(max_length=1) # 5 estrelas
+class Avaliar(models.Model):
+    Buscar = models.ForeignKey(Buscar, on_delete=models.CASCADE, related_name="avaliacoes")
+    nota = models.IntegerField(
+        validacao=[MinValueValidator(1),MaxValueValidator(5)] # 5 estrelas
+    ) 
     comentario = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return f" Avaliação: {self.titulo} - {self.nota} estrelas"
+    
 
+class Add_Biblioteca(models.Model):
+    titulo = models.CharField(max_length=200)
+    desenvolvedor = models.CharField(max_length=200, blank=False)
+    ano_lancamento = models.DateField(blank=False,null=False)
+    genero = models.CharField(max_length=50, blank=False)
+
+    def __str__(self):
+        return f"{self.titulo} adicionado na biblioteca!"
