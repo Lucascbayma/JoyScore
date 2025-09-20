1d640d7e69f79620b094d072c8671822ba537864
Lucascbayma <Lucasbayma31@gmail.com>
2025-09-20 13:42:41 -0300

    Melhorando identação das classes e arrumando erros da classe avaliar e adicionar jogo

--- jogos/models.py
+++ jogos/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Buscar(models.Model):
-    titulo = models.CharField(max_length=200)
+class Jogo(models.Model):
+    titulo = models.CharField(max_length=200, unique=True)
     desenvolvedor = models.CharField(max_length=200, blank=True)
     ano_lancamento = models.DateField(blank=True,null=True)
     genero = models.CharField(max_length=50, blank=True)
@@ -12,21 +14,24 @@ class Buscar(models.Model):
         return self.titulo
     
 class Avaliar(models.Model):
-    Buscar = models.ForeignKey(Buscar, on_delete=models.CASCADE, related_name="avaliacoes")
+    Buscar = models.ForeignKey(Jogo, on_delete=models.CASCADE, related_name="avaliacoes")
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE) # Isso é pra vincular a avaliação ao usuário!
     nota = models.IntegerField(
-        validacao=[MinValueValidator(1),MaxValueValidator(5)] # 5 estrelas
+        validacao = [MinValueValidator(1),MaxValueValidator(5)] # 5 estrelas
     ) 
-    comentario = models.CharField(max_length=1000)
+    comentario = models.CharField(max_length=1000, blank=True)
 
     def __str__(self):
-        return f" Avaliação: {self.titulo} - {self.nota} estrelas"
+        return f"Avaliação do jogo {self.Buscar.titulo} por {self.usuario.username} - {self.nota} estrelas"
     
 
 class Add_Biblioteca(models.Model):
-    titulo = models.CharField(max_length=200)
-    desenvolvedor = models.CharField(max_length=200, blank=False)
-    ano_lancamento = models.DateField(blank=False,null=False)
-    genero = models.CharField(max_length=50, blank=False)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    jogo = models.ForeignKey(Jogo, on_delete=models.CASCADE)
+    data_adicionado = models.DateTimeField(auto_now_add=True)
+
+    class verificar:
+        unico = ('usuario', 'jogo') # Isso vai servir pra que o user não add o mesmo jogo 2 vzs na biblioteca 
 
     def __str__(self):
-        return f"{self.titulo} adicionado na biblioteca!"
+        return f"{self.jogo.titulo} foi adicionado na sua biblioteca!"
