3be0e919af45b9310b68077fe9807610fd7d0688
Lucascbayma <Lucasbayma31@gmail.com>
2025-09-20 14:20:37 -0300

    Iniciando implementação da views.py buscar_jogos

--- jogos/views.py
+++ jogos/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Jogo
+from django.db.models import Q
 
 # Create your views here.
+
+def buscar_jogos(request):
+    pesquisa = request.GET.get('q')
+    jogos = Jogo.objects.all()
+
+    if pesquisa:
+        jogos = jogos.filter(Q(titulo__icontains=pesquisa) | Q(desenvolvedor__icontains=pesquisa) | Q(genero__icontains=pesquisa)).distinct() # Esse distinct é pra que não tenha resultados duplicados
+    context = {'jogos': jogos, 'pesquisa': pesquisa,}
+
+    return render(request, 'buscar_jogos.hmtl', context)
