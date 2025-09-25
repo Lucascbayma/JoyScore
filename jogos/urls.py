52aca4105363bac8f6b300349db3a405036d5561
Lucascbayma <Lucasbayma31@gmail.com>
2025-09-24 21:21:12 -0300

    Melhorando importação na urls.py da pasta jogos

--- jogos/urls.py
+++ jogos/urls.py
@@ -1,18 +1,23 @@
 from django.urls import path
-from . import views
+from .views import (
+    buscar_jogos,
+    minha_biblioteca,
+    adicionar_biblioteca,
+    avaliar
+)
 
 app_name = 'jogos' 
 
 urlpatterns = [
     # PATH para história 1
-    path('buscar/', views.buscar_jogos, name='buscar_jogos'),
+    path('buscar/', buscar_jogos, name='buscar_jogos'),
 
     # PATH para mostrar Jogos do usuário
-    path('minha-biblioteca/', views.minha_biblioteca, name='minha_biblioteca'),
+    path('minha-biblioteca/', minha_biblioteca, name='minha_biblioteca'),
     
     # PATH para História 2
-    path('adicionar/<int:jogo_id>/', views.adicionar_biblioteca, name='adicionar_biblioteca'),
+    path('adicionar/<int:jogo_id>/', adicionar_biblioteca, name='adicionar_biblioteca'),
 
     # História 3: Avaliar um Jogo
-    path('avaliar/<int:jogo_id>/', views.avaliar, name='avaliar'),
+    path('avaliar/<int:jogo_id>/', avaliar, name='avaliar'),
 ]
