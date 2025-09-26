2743e6cee3300a57aa97126f3b2b7a53985a8bc7
Lucascbayma <Lucasbayma31@gmail.com>
2025-09-26 12:21:20 -0300

    Definindo os modelos para o admin.py

--- jogos/admin.py
+++ jogos/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Jogo, Avaliar, Add_Biblioteca
+
+class JogoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'desenvolvedor', 'genero', 'ano_lancamento')
+    search_fields = ('titulo', 'desenvolvedor', 'genero')
+    list_filter = ('genero', 'desenvolvedor')
+    ordering = ('titulo',)
+
+class AvaliarAdmin(admin.ModelAdmin):
+    list_display = ('Jogo', 'usuario', 'nota', 'comentario')
+    list_filter = ('Jogo', 'usuario', 'nota')
+    list_editable = ('nota',)
+    ordering = ('-id',) 
+
+class AddBibliotecaAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'jogo', 'data_adicionado')
+    list_filter = ('data_adicionado',)
+    search_fields = ('jogo__titulo', 'usuario__username')
+    readonly_fields = ('data_adicionado',)
+
+admin.site.register(Jogo, JogoAdmin)
+admin.site.register(Avaliar, AvaliarAdmin)
+admin.site.register(Add_Biblioteca, AddBibliotecaAdmin)
