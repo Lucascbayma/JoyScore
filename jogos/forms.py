399ffd68d7a88bd8e4646f259528d451b312d94d
Lucascbayma <Lucasbayma31@gmail.com>
2025-09-28 15:38:13 -0300

    Deletando forms.py

--- jogos/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Avaliar
-
-class AvaliacaoForm(forms.ModelForm):
-    class Meta:
-        model = Avaliar 
-        
-        fields = ['nota', 'comentario'] 
