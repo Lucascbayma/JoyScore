fe9a7c1577f5692fd34008aed66d291601bf6756
Lucascbayma <Lucasbayma31@gmail.com>
2025-10-16 23:18:38 -0300

    Adicionado o webhook na urls.py da pasta raiz

--- joyscore/urls.py
+++ joyscore/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from core.views import github_webhook
 
 urlpatterns = [
     # Rota para o painel de administração do Django
@@ -12,4 +13,7 @@ urlpatterns = [
     # Rota para o seu app original (RAWG)
     # O path '' diz ao Django que este app cuida da PÁGINA INICIAL e outras rotas
     path('', include('jogos.urls')),
+
+    # Adicionando o webhook na raiz
+    path('webhook/github/', github_webhook, name='github_webhook'),
 ]
